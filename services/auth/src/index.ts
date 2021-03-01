9ed9136d9b26d23595361f2e0579baf405a97f34
valdestron <valdas.mazrimas@gmail.com>
2021-03-01 11:33:15 +0200

    remove tested auth0

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -14,9 +14,9 @@ const start = async () => {
   try {
     // TODO: this runs one time during migration only
     // await setupAuth0Clients({
-    //   clientId: '5CrcRF5dwtRk1ScjhRRrG4vSbQko571I',
-    //   clientSecret: 'IWLEDMZ-6epLMLy5-HiunPZVC08hRIv2DveRneU04tu-gsaU1S3NgY2x423FjBiC',
-    //   domain: 'dev-2whak4bt.eu.auth0.com',
+    //   clientId: '??',
+    //   clientSecret: ' ?? ',
+    //   domain: '??',
     // })
     // await installAuth0Apps({
     //   callbacks: ['http://localhost:3000', 'http://localhost:3000/login'],
